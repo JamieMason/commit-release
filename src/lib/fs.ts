a9db11744a8ce21086ff2a8ac821081864a0e496
Jamie Mason <jamie@foldleft.io>
2017-12-24 19:04:29 +0000

    fix(core): catch ENOENT errors when deleting files

--- src/lib/fs.ts
+++ src/lib/fs.ts
@@ -4,7 +4,13 @@ export const createWriteStream = fsCreateWriteStream;
 
 export const unlink = (path: string): Promise<void> =>
   new Promise((resolve, reject) => {
-    fsUnlink(path, (err: Error | null) => (err ? reject(err) : resolve()));
+    fsUnlink(path, (err: Error | null) => {
+      if (err && err.message.indexOf('ENOENT') === -1) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
   });
 
 export const writeFile = (path: string, contents: string, options?: object): Promise<void> =>
